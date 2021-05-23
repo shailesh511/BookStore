f5ab7206e6d227b8cfe49c451d0d080218414b51
Shailesh <shaileshtripathi511@gmail.com>
2021-05-23 12:10:39 +0530

    Added Categories model to Db

--- BookHub.DataAccess/Data/ApplicationDbContext.cs
+++ BookHub.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace BookHub.DataAccess.Data
             : base(options)
         {
         }
+       public DbSet<Category> Categories { get; set; }
+        
     }
 }
